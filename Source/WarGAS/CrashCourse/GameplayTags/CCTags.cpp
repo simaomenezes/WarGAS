369f167f7bebc953edb9ae5556a4869f7be27a5e
Simão Neto <simaomenezesneto@gmail.com>
2026-06-03 22:42:40 +0100

    feat: We did Native Gameplay Tags

---
+++ Source/WarGAS/CrashCourse/GameplayTags/CCTags.cpp
@@ -0,0 +1,11 @@
+﻿// War GAS - SIMAO
+
+#include "CCTags.h"
+
+namespace CCTags
+{
+	namespace CCAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "CCTags.CCAbilities.Primary", "Tag for the Primary Ability")
+	}
+}
---
+++ Source/WarGAS/CrashCourse/GameplayTags/CCTags.h
@@ -0,0 +1,14 @@
+﻿// War GAS - SIMAO
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace CCTags
+{
+	namespace CCAbilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary);
+	}
+}
