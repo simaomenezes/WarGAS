6652ea557f36c85bc7da6e38b272637699b3d9d2
Simão Neto <simaomenezesneto@gmail.com>
2026-06-04 15:31:17 +0100

    feat: Created Overlap Test for collision

---
+++ Source/WarGAS/CrashCourse/AbilitySystem/Abilities/Player/CC_Primary.h
@@ -0,0 +1,31 @@
+﻿// War GAS - SIMAO
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WarGAS/CrashCourse/AbilitySystem/Abilities/CC_GameplayAbility.h"
+#include "CC_Primary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARGAS_API UCC_Primary : public UCC_GameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Crash|Abilities")
+	void HitBoxOverlapTest();
+	
+private:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxRadius = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxForwardOffset = 200.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxElevationOffset = 20.0f;
+};
