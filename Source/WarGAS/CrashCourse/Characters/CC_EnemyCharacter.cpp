84f6dbb3cfb89f111e8ddb130d157819e79a7bca
Simão Neto <simaomenezesneto@gmail.com>
2026-06-04 12:07:35 +0100

    feat: Created Enemy Class and add enemy mesh to game

---
+++ Source/WarGAS/CrashCourse/Characters/CC_EnemyCharacter.cpp
@@ -0,0 +1,35 @@
+﻿// War GAS - SIMAO
+
+
+#include "CC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+ACC_EnemyCharacter::ACC_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* ACC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ACC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (!IsValid(GetAbilitySystemComponent())) return;
+	
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+	
+	if (!HasAuthority()) return;
+	
+	GiveStartUpAbilities();
+	
+}
---
+++ Source/WarGAS/CrashCourse/Characters/CC_EnemyCharacter.h
@@ -0,0 +1,26 @@
+﻿// War GAS - SIMAO
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CC_BaseCharacter.h"
+#include "CC_EnemyCharacter.generated.h"
+
+UCLASS()
+class WARGAS_API ACC_EnemyCharacter : public ACC_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACC_EnemyCharacter();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
