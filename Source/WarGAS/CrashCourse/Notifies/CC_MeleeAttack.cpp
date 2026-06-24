2554a251f44c4e8d6ff5ef1e4d4f5514e52e5668
Simão Neto <simaomenezesneto@gmail.com>
2026-06-24 16:46:18 +0100

    feat: Converting the Notify State to Code

---
+++ Source/WarGAS/CrashCourse/Notifies/CC_MeleeAttack.cpp
@@ -0,0 +1,92 @@
+﻿// War GAS - SIMAO
+
+
+#include "CC_MeleeAttack.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffectTypes.h"
+#include "KismetTraceUtils.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "WarGAS/CrashCourse/Characters/CC_PlayerCharacter.h"
+#include "WarGAS/CrashCourse/GameplayTags/CCTags.h"
+
+void UCC_MeleeAttack::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime,
+                                 const FAnimNotifyEventReference& EventReference)
+{
+	
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+	if (!IsValid(MeshComp)) return;
+	if (!IsValid(MeshComp->GetOwner())) return;
+
+	TArray<FHitResult> Hits = PerformSphereTrace(MeshComp);
+	SendEventsToActors(MeshComp, Hits);
+}
+
+TArray<FHitResult> UCC_MeleeAttack::PerformSphereTrace(USkeletalMeshComponent* MeshComp) const
+{
+	TArray<FHitResult> OutHits;
+
+	const FTransform SocketTransform = MeshComp->GetSocketTransform(SocketName);
+	const FVector Start = SocketTransform.GetLocation();
+	const FVector ExtendedSocketDirection = UKismetMathLibrary::GetForwardVector(SocketTransform.GetRotation().Rotator()) * SocketExtensionOffset;
+	const FVector End = Start - ExtendedSocketDirection;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(MeshComp->GetOwner());
+	FCollisionResponseParams ResponseParams;
+	ResponseParams.CollisionResponse.SetAllChannels(ECR_Ignore);
+	ResponseParams.CollisionResponse.SetResponse(ECC_Pawn, ECR_Block);
+	UWorld* World = GEngine->GetWorldFromContextObject(MeshComp, EGetWorldErrorMode::LogAndReturnNull);
+	if (!IsValid(World)) return OutHits;
+	
+	bool const bHit = World->SweepMultiByChannel(
+		OutHits,
+		Start,
+		End,
+		FQuat::Identity,
+		ECC_Visibility,
+		FCollisionShape::MakeSphere(SphereTraceRadius),
+		Params,
+		ResponseParams);
+
+	if (bDrawDebugs)
+	{
+		DrawDebugSphereTraceMulti(
+			World,
+			Start,
+			End,
+			SphereTraceRadius,
+			EDrawDebugTrace::ForDuration,
+			bHit,
+			OutHits,
+			FColor::Green,
+			FColor::Red,
+			5.f);
+	}
+
+	return OutHits;
+}
+
+void UCC_MeleeAttack::SendEventsToActors(USkeletalMeshComponent* MeshComp, const TArray<FHitResult>& Hits) const
+{
+	for (const FHitResult& Hit : Hits)
+	{
+		ACC_PlayerCharacter* PlayerCharacter = Cast<ACC_PlayerCharacter>(Hit.GetActor());
+		if (!IsValid(PlayerCharacter)) continue;
+		if (!PlayerCharacter->IsAlive()) continue;
+		UAbilitySystemComponent* ASC = PlayerCharacter->GetAbilitySystemComponent();
+		if (!IsValid(ASC)) continue;
+
+		FGameplayEffectContextHandle ContextHandle = ASC->MakeEffectContext();
+		ContextHandle.AddHitResult(Hit);
+
+		FGameplayEventData Payload;
+		Payload.Target = PlayerCharacter;
+		Payload.ContextHandle = ContextHandle;
+		Payload.Instigator = MeshComp->GetOwner();
+
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(MeshComp->GetOwner(), CCTags::Events::Enemy::MeleeTraceHit, Payload);
+	}
+}
---
+++ Source/WarGAS/CrashCourse/Notifies/CC_MeleeAttack.h
@@ -0,0 +1,36 @@
+﻿// War GAS - SIMAO
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "CC_MeleeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARGAS_API UCC_MeleeAttack : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+	
+public:
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Crash|Debugs")
+	bool bDrawDebugs = true;
+
+	UPROPERTY(EditAnywhere, Category = "Crash|Socket")
+	FName SocketName{"FX_Trail_01_R"};
+
+	UPROPERTY(EditAnywhere, Category = "Crash|Socket")
+	float SocketExtensionOffset{40.f};
+
+	UPROPERTY(EditAnywhere, Category = "Crash|Socket")
+	float SphereTraceRadius{60.f};
+
+	TArray<FHitResult> PerformSphereTrace(USkeletalMeshComponent* MeshComp) const;
+	void SendEventsToActors(USkeletalMeshComponent* MeshComp, const TArray<FHitResult>& Hits) const;
+};
