bd31484e8017a32a308dee705f4aabb0a90ddf3e
Simão Neto <simaomenezesneto@gmail.com>
2026-06-24 11:12:50 +0100

    feat: making Projectile Class and spawn to enemy weapon

---
+++ Source/WarGAS/CrashCourse/GameObjects/CC_Projectile.cpp
@@ -0,0 +1,42 @@
+﻿// War GAS - SIMAO
+
+
+#include "CC_Projectile.h"
+
+#include "AbilitySystemComponent.h"
+#include "GameplayEffectTypes.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "WarGAS/CrashCourse/Characters/CC_PlayerCharacter.h"
+
+// Sets default values
+ACC_Projectile::ACC_Projectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+
+	bReplicates = true;
+}
+
+void ACC_Projectile::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	ACC_PlayerCharacter* PlayerCharacter = Cast<ACC_PlayerCharacter>(OtherActor);
+	if (!IsValid(PlayerCharacter)) return;
+	if (!PlayerCharacter->IsAlive()) return;
+	
+	UAbilitySystemComponent* AbilitySystemComponent = PlayerCharacter->GetAbilitySystemComponent();
+	if (!IsValid(AbilitySystemComponent) || !HasAuthority()) return;
+	
+	FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
+	FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.f, ContextHandle);
+	
+	//TODO Use the Damage variable for this amount of damage to couse
+	
+	AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	
+	SpawnImpactEffect();
+	Destroy();
+}
---
+++ Source/WarGAS/CrashCourse/GameObjects/CC_Projectile.h
@@ -0,0 +1,35 @@
+﻿// War GAS - SIMAO
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CC_Projectile.generated.h"
+
+class UGameplayEffect;
+class UProjectileMovementComponent;
+
+UCLASS()
+class WARGAS_API ACC_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACC_Projectile();
+	
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Crash|Damage", meta=(ExposeOnSpawn, ClampMin="0.0"))
+	float Damage{10.f};
+	
+	UFUNCTION(BlueprintImplementableEvent, Category="Crash|Projectile")
+	void SpawnImpactEffect();
+private:
+	
+	UPROPERTY(VisibleAnywhere, Category= "Crash|Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+	
+	UPROPERTY(EditDefaultsOnly, Category= "Crash|Damage")
+	TSubclassOf<UGameplayEffect> DamageEffect;
+};
