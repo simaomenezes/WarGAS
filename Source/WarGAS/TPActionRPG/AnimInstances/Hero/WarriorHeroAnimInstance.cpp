63226e03fa070352c2100dcb26ef743dfc060ce4
Simão Neto <simaomenezesneto@gmail.com>
2026-06-30 22:02:17 +0100

    feat: Making flow to anim relax idle

--- Source/WarGAS/TPActionRPG/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/WarGAS/TPActionRPG/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -2,3 +2,32 @@
 
 
 #include "WarriorHeroAnimInstance.h"
+
+#include "WarGAS/TPActionRPG/Characters/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	
+	if (bHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElpasedTime += DeltaSeconds;
+
+		bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxtStateThreshold);
+	}
+}
--- Source/WarGAS/TPActionRPG/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/WarGAS/TPActionRPG/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "WarGAS/TPActionRPG/AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -13,4 +14,21 @@ UCLASS()
 class WARGAS_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
+	
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Warrior|AnimData|Refrences")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Warrior|AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Warrior|AnimData|LocomotionData")
+	float EnterRelaxtStateThreshold = 5.f;
+
+	float IdleElpasedTime;
 };
